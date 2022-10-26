ba7f3dfbf653b57f4b029aa152b8069b70241703
ViewHolder74 <viewholder74@gmail.com>
2022-10-26 08:10:52 -0700

    feat: add profile model and signals

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
+from enum import auto, unique
 from django.db import models
+import uuid 
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- apps/users/manager.py
+++ apps/users/manager.py
@@ -30,9 +30,9 @@ class CustomUserManager(BaseUserManager):
 
         user = self.model(
             username = username,
-            email = email,
             first_name = first_name,
             last_name = last_name,
+            email = email,
             **extra_fields
         )
 
@@ -42,7 +42,7 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    def create_superuser(self, username, first_name, last_name, password, **extra_fields):
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
@@ -54,7 +54,7 @@ class CustomUserManager(BaseUserManager):
             raise ValueError(_("Superusers must have is superuser=True"))
 
         if not password:
-            raise ValueError(_("Superusers must have a password"))
+            raise ValueError(_("Superuse rs must have a password"))
 
         if email:
             email = self.normalize_email(email)
@@ -62,12 +62,6 @@ class CustomUserManager(BaseUserManager):
         else:
             raise ValueError(_("Admin Account: An email address is required"))
 
-        user = self.create_user(
-            username, 
-            first_name,
-            last_name,
-            password,
-            **extra_fields
-        )
+        user = self.create_user(username, first_name, last_name, email, password, **extra_fields) 
         user.save(using=self._db)
         return user
