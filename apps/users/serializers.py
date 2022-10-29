a685d7bc14477be4be87f6ab433ef572f31c8b80
ViewHolder74 <viewholder74@gmail.com>
2022-10-29 13:57:08 -0700

    feat: register, login, update profile

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -15,6 +15,7 @@ class UserSerializer(serializers.ModelSerializer):
     top_seller = serializers.BooleanField(source="profile.top_seller")
     first_name = serializers.SerializerMethodField()
     last_name = serializers.SerializerMethodField()
+
     full_name = serializers.SerializerMethodField(source="get_full_name")
 
     class Meta:
