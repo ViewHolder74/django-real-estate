8c6cf0845c92b094e206614beb35b2f2de88095b
ViewHolder74 <viewholder74@gmail.com>
2022-11-01 18:01:39 -0700

    feat: docker and compose setup

--- apps/enquiries/models.py
+++ apps/enquiries/models.py
@@ -1,6 +1,4 @@
-from email import message
-from email.policy import default
-from tabnanny import verbose
+
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
