9b46547a2beeca7d1365ccad38ba53469f90ceca
ViewHolder74 <viewholder74@gmail.com>
2022-11-08 15:06:21 -0800

    feat: add celery and flower

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -3,13 +3,13 @@
 
 from real_estate.settings.base import *
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_USE_TLS = True
 EMAIL_PORT = env("EMAIL_PORT")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAUT_FROM_EMAIL = "info@real-estate.com"
+DEFAUT_FROM_EMAIL = "info@real-estate.com" 
 DOMAIN = env("DOMAIN")
 SITE_NAME = "Real Estate"
 
@@ -23,3 +23,8 @@ DATABASES = {
         "PORT": env("PG_PORT"),
     }
 }
+
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = env("CELERY_BACKEND")
+CELERY_TIMEZONE = "Africa/Blantyre"
